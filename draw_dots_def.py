e32b07c9ce686831b5a4490e9771876339e661ee
giuliacaiani <85154867+giuliacaiani@users.noreply.github.com>
2021-06-11 11:56:08 +0200

    calibrazione

---
+++ draw_dots_def.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+import pygame as pg
+
+
+# In[18]:
+
+
+res_x = 900
+res_y = 640
+off_set = 100
+
+
+# In[20]:
+
+
+pg.init()
+screen= pg.display.set_mode((res_x,res_y))
+pg.display.set_caption('CALIBRAZIONE')
+
+#COLORI
+RED = (255,0,0)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+CYAN = (0,255,255)
+
+#ALTO SX
+pg.draw.circle(screen, CYAN, (off_set, off_set), 50, 5)
+pg.draw.circle(screen, CYAN, (off_set, off_set), 5, 5)
+
+pg.draw.circle(screen, CYAN, (res_x-off_set, off_set), 50, 5)
+pg.draw.circle(screen, CYAN, (res_x-off_set, off_set), 5, 5)
+
+pg.draw.circle(screen, CYAN, (res_x-off_set, res_y-off_set), 50, 5)
+pg.draw.circle(screen, CYAN, (res_x-off_set, res_y-off_set), 5, 5)
+
+pg.draw.circle(screen, CYAN, (off_set, res_y-off_set), 50, 5)
+pg.draw.circle(screen, CYAN, (off_set, res_y-off_set), 5, 5)
+
+pg.display.flip()
+
+
+# In[ ]:
+
+
+
+
