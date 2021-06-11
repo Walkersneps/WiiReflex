8c82fc68b818f9250b16cd5486beaaee5608dca0
Walkersneps <walter@sneps.xyz>
2021-06-11 12:31:16 +0200

    Test di wiimote/infrarossi rapido

---
+++ ir_test.py
@@ -0,0 +1,28 @@
+import cwiid
+import time
+
+def dry(w: cwiid.Wiimote):
+    print(wm.state['ir_src'])
+    for sorg in w.state['ir_src']: # per ogni sorgente IR letta dal wiimote
+        if sorg is not None:
+            posix = sorg['pos'] # tupla coordinate
+            print(posix[0], ', ', posix[1], ', ', sorg['size'])
+
+
+print("Connetto...")
+wm = cwiid.Wiimote()
+print("Connessione effettuata")
+wm.led = 3
+time.sleep(1)
+wm.led = 5
+
+wm.rpt_mode = cwiid.RPT_IR # chiedo che mi mandi i dati degli IR
+print(wm.state)
+
+
+for i in range(1000):
+    #print(wm.state['ir_src'])
+    dry(wm)
+    time.sleep(0.02)
+
+print("Fine!\n")
